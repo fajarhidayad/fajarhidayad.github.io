a1ccb6b4f9034f28372de0ee085496161919aca2
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2019-11-03 20:42:20 +0700

    merubah font

--- javascript.js
+++ javascript.js
@@ -12,6 +12,22 @@
     modal.style.display = 'block';
     }
 
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#image_upload_preview').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#inputFile").change(function () {
+        readURL(this);
+    });
+
     // Close
     function closeModal() {
     modal.style.display = 'none';
