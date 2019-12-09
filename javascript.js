fefe1ebdd3ff878024a6b6fe1c07ba15ad23ed85
Risqi Faisol <faisolakbar0@gmail.com>
2019-12-09 21:29:52 +0700

    nambah nambah

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@
     function bukaModal() {
     modal.style.display = 'block';
     }
-
+    //untuk preview foto
     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -39,3 +39,39 @@
         modal.style.display = 'none';
     }
     }
+
+//untuk cek huruf ato angka
+    function checkLetter()
+        {
+            var validasiHuruf = /^[a-zA-Z ]+$/;
+            var namaKota = document.getElementById("nama");
+            
+            if (namaKota.value.match(validasiHuruf)) {
+                alert("Data Berhasil Di Input");
+            } else {
+                alert("Nama Wajib Huruf!");
+                
+            }
+        }
+
+//jika tipe file bukan image
+function validasiEkstensi(){
+    var inputFile = document.getElementById('inputFile');
+    var pathFile = inputFile.value;
+    var ekstensiOk = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+    if(!ekstensiOk.exec(pathFile)){
+        alert('Silakan upload file yang memiliki ekstensi .jpeg/.jpg/.png/.gif');
+        inputFile.value = '';
+        return false;
+    }
+}
+
+$("#nana").click(function(){
+    var a = $("#namaL").val();
+    var b = $("#lokasi").val();
+    if (a == "" && b == "") {
+        alert('Data Harus Diisi');
+    }else{
+        
+    }
+});
